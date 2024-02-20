83ca8757c8b6974fc2c6b82c9fed7a950f779c56
Brady Georgen <brady@familiarcat.com>
2024-02-19 22:12:23 -0600

    generating from base folder

--- src/create_ae_project/create_ae_project.sh
+++ src/create_ae_project/create_ae_project.sh
@@ -11,7 +11,9 @@ rm -r $PROJECT_DIR; mkdir $PROJECT_DIR
 mkdir -p $PROJECT_DIR/scripts/utilities
 mkdir -p $PROJECT_DIR/scripts/modules
 cp ./src/create_ae_project/main.jsx $PROJECT_DIR/scripts/main.jsx
-# cp ./scripts/modules/compositionCreator.jsx $PROJECT_DIR/scripts/modules/compositionCreator.jsx 
+cp ./src/create_ae_project/scripts/modules/compositionCreator.jsx $PROJECT_DIR/scripts/modules/compositionCreator.jsx 
+cp ./src/create_ae_project/scripts/utilities/helpers.jsx $PROJECT_DIR/scripts/utilities/helpers.jsx 
+
 # cp ./scripts/utilities/helpers.jsx $PROJECT_DIR/scripts/utilities/helpers.jsx 
 
 mkdir -p $PROJECT_DIR/sources/images
